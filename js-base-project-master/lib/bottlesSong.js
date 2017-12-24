93822bc09a40e0f77139276c9726b086f3d3076b
yuqinglove189 <32956274+yuqinglove189@users.noreply.github.com>
2017-12-24 14:46:16 +0800

    1

--- js-base-project-master/lib/bottlesSong.js
+++ js-base-project-master/lib/bottlesSong.js
@@ -1,15 +1,22 @@
 function bottlesSong(number){
     let result="";
-    if(number > 1 && number < 100){
-        result=number+" bottles of beer on the wall, "+number+" bottles of beer.\n" +
-            "Take one down and pass it around, "+ (number-1) +" bottles of beer on the wall.\n"
+    if(number > 2 && number < 100){
+        result=number+" bottles of beer on the wall,"+number+" bottles of beer.\n" +
+            "Take one down and pass it around, "+ (number-1) +" bottles of beer on the wall.\n";
         return result;
     }
-    if( number = 1){
-        result="Take one down and pass it around, 1 bottle of beer on the wall.\n" +
-            "1 bottle of beer on the wall, 1 bottle of beer.\n" +
-            "Take one down and pass it around, no more bottles of beer on the wall.\n" +
-            "No more bottles of beer on the wall, no more bottles of beer.\n" +
+    if(number === 2 ){
+        result= number+" bottles of beer on the wall, "+number+" bottles of beer.\n" +
+            "Take one down and pass it around, "+ (number-1) +" bottle of beer on the wall.\n";
+        return result;
+    }
+    if( number === 1){
+        result = number+" bottle of beer on the wall, "+number+" bottle of beer.\n" +
+            "Take one down and pass it around, no more bottles of beer on the wall.\n";
+        return result;
+    }
+    if( number === 0){
+        result="No more bottles of beer on the wall, no more bottles of beer.\n"+
             "Go to the store and buy some more, 99 bottles of beer on the wall.";
         return result;
     }
--- js-base-project-master/spec/main_spec.js
+++ js-base-project-master/spec/main_spec.js
@@ -14,35 +14,31 @@ describe("bottles", function(){
     it("return when bottles 99", function(){
         let number = 99;
         let result = bottlesSong(number);
-        let res = number+" bottles of beer on the wall, "+number+" bottles of beer.\n" +
-            "Take one down and pass it around, "+ (number-1) +" bottles of beer on the wall.\n"
+        let res = number+" bottles of beer on the wall," +number+" bottles of beer.\n" +
+            "Take one down and pass it around, "+ (number-1) +" bottles of beer on the wall.\n";
         expect(result).to.equal(res);
     });
 
-    it("return when bottles 50 ", function(){
-        let number = 50;
+    it("return when bottles 2 ", function(){
+        let number = 2;
         let result = bottlesSong(number);
         let res = number+" bottles of beer on the wall, "+number+" bottles of beer.\n" +
-            "Take one down and pass it around, "+ (number-1) +" bottles of beer on the wall.\n"
-        expect(result).to.equal(res);
-    });
-
-    it("return when bottles 77 ", function(){
-        let number = 77;
-        let result = bottlesSong(number);
-        let res = number+" bottles of beer on the wall, "+number+" bottles of beer.\n" +
-            "Take one down and pass it around, "+ (number-1) +" bottles of beer on the wall.\n"
+            "Take one down and pass it around, "+ (number-1) +" bottle of beer on the wall.\n";
         expect(result).to.equal(res);
     });
 
     it("return when bottles 1", function(){
         let number = 1;
         let result = bottlesSong(number);
-        let res = "Take one down and pass it around, 1 bottle of beer on the wall.\n" +
-            "1 bottle of beer on the wall, 1 bottle of beer.\n" +
-            "Take one down and pass it around, no more bottles of beer on the wall.\n" +
-            "No more bottles of beer on the wall, no more bottles of beer.\n" +
-            "Go to the store and buy some more, 99 bottles of beer on the wall."
+        let res =number+" bottle of beer on the wall, "+number+" bottle of beer.\n" +
+            "Take one down and pass it around, no more bottles of beer on the wall.\n" ;
+        expect(result).to.equal(res);
+    });
+    it("return when bottles 0", function(){
+        let number = 0;
+        let result = bottlesSong(number);
+        let res = "No more bottles of beer on the wall, no more bottles of beer.\n"+
+            "Go to the store and buy some more, 99 bottles of beer on the wall.";
         expect(result).to.equal(res);
     });
 
